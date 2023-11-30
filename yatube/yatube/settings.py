c3b3e039d6ad29cc77f32ac270dcac04b88038b7
vitalik <sandwich.htc@gmail.com>
2023-11-30 18:52:37 +0300

    Update setting, added allowed hosts

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -28,10 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '[::1]',
-    'testserver',
-    'www.VitalikScherbakov.pythonanywhere.com',
-    'VitalikScherbakov.pythonanywhere.com',
+    'mini-socialnetwork.ddns.net'
 ] 
 
 
