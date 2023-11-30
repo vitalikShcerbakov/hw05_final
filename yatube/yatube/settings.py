118cae4a17d2e0cebaafaab2cb2694741b79744c
vitalik <sandwich.htc@gmail.com>
2023-11-30 18:55:34 +0300

    update setting, db for container

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -92,15 +92,12 @@ WSGI_APPLICATION = 'yatube.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/data/db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
